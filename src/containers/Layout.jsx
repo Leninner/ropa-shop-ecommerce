6ede0338da82abbb9db8c9a8852ac6ea9e24350e
leninner <mazabandalenin180@gmail.com>
2021-11-15 19:47:42 -0500

    Añado config iniciañ

---
+++ src/containers/Layout.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Layout = ({ children }) => {
+  return <div className='App'>{children}</div>;
+};
+
+export { Layout };
---
+++ src/routes/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from '../containers/Layout';
+import { Home } from '../pages/Home';
+import { PageNotFound } from '../pages/PageNotFound';
+import '../assets/styles/main.css';
+
+const App = () => (
+  <BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </Layout>
+  </BrowserRouter>
+);
+
+export { App };
