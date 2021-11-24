10cddc0c8aaa9b9bbb599776aef1a9cca50b19b7
leninner <mazabandalenin180@gmail.com>
2021-11-23 21:12:32 -0500

    Añado classnames

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { CardItem } from '../components/CardItem';
 import '../assets/styles/components/ProductList.scss';
+import classNames from 'classnames';
 
-const ProductList = () => {
+const ProductList = ({ isLogin, isRegister }) => {
+  const productList = classNames('product-list', {
+    isLogin,
+    isRegister,
+  });
+
   return (
-    <section className='product-list'>
+    <section className={productList}>
       <div className='product-list__container'>
         <CardItem />
         <CardItem />
