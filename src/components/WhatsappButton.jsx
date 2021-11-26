40837c6f72d7044da89ab77f1fa31c16cae67628
leninner <mazabandalenin180@gmail.com>
2021-11-26 17:22:36 -0500

    [UPDATE] files

--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../assets/styles/components/WhatsappButton.scss';
+import { connect } from 'react-redux';
 
-export const WhatsappButton = () => (
-  <>
-    {' '}
-    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' />
-    <a href='https://walink.co/96e5fc' className='float' target='_blank' rel='noreferrer'>
-      <i className='fa fa-whatsapp my-float'></i>
-    </a>
-  </>
-);
+const WhatsappButton = () => {
+  return (
+    <>
+      {' '}
+      <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' />
+      <a href='https://walink.co/96e5fc' className='float' target='_blank' rel='noreferrer'>
+        <i className='fa fa-whatsapp my-float'></i>
+      </a>
+    </>
+  );
+};
+
+export default connect(null, null)(WhatsappButton);
--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
-import { WhatsappButton } from '../components/WhatsappButton';
+import WhatsappButton from '../components/WhatsappButton';
 
 const Layout = ({ children }) => {
   return (
