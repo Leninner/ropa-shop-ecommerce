1385114b74d5140043a051ee69b5f77020d54e50
leninner <mazabandalenin180@gmail.com>
2021-11-26 18:49:48 -0500

    [UPDATE] añado solución para enviar los pedidos a Whatsapp

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import '../assets/styles/components/Checkout.scss';
 
-const Checkout = ({ sumaTotal }) => {
+const Checkout = ({ sumaTotal, cart }) => {
+  let encodeText = 'Hola mi pedido es: \n';
+
+  cart.map((value) => (encodeText += ` - ${value.name} ${value.price}\n`));
+
+  const link = `https://wa.me/593987223910?text=${encodeURIComponent(encodeText)}`;
+
   return (
     <div className='checkout'>
       <div className='checkout-total'>
         <h4>Total</h4>
         <p className='checkout__price'>${sumaTotal}</p>
       </div>
-      <button className='checkout__button'>Iniciar proceso de Pago</button>
+      <button className='checkout__button'>
+        <a href={link} target='_blank' rel='noreferrer'>
+          Iniciar proceso de Pago
+        </a>
+      </button>
     </div>
   );
 };
 
-export default Checkout;
+const mapStateToProps = (state) => {
+  return {
+    cart: state.cart,
+  };
+};
+
+export default connect(mapStateToProps, null)(Checkout);
