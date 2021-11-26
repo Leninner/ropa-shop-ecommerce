d82ad71d15bd3d8930bc0423b68c2514cd6f9e2c
leninner <mazabandalenin180@gmail.com>
2021-11-26 16:35:54 -0500

    [UPDATE] files

--- src/index.jsx
+++ src/index.jsx
@@ -21,60 +21,16 @@ const initialState = {
       name: 'Sudadera New York',
       price: 15,
       description: 'Sudaderas con capucha estilos únicos y de excelente calidad',
-      image: hoddie,
+      image:
+        'https://media.istockphoto.com/photos/smiling-man-outdoors-in-the-city-picture-id1179420343?b=1&k=20&m=1179420343&s=170667a&w=0&h=xBR4ZQbRC1Ub_5u_pZnG-omh0k-W8QDQgXxda5MCDT8=',
     },
     {
       id: 98,
       name: 'Sudadera New York',
       price: 15,
       description: 'Sudaderas con capucha estilos únicos y de excelente calidad',
-      image: hoddie,
-    },
-  ],
-  womens: [
-    {
-      id: 1,
-      name: 'Sudadera New York',
-      price: 15,
-      description: 'Sudaderas con capucha estilos únicos y de excelente calidad',
-      image: hoddie,
-    },
-    {
-      id: 2,
-      name: 'Sudadera New York',
-      price: 15,
-      description: 'Sudaderas con capucha estilos únicos y de excelente calidad',
-      image: hoddie,
-    },
-    {
-      id: 3,
-      name: 'Sudadera New York',
-      price: 15,
-      description: 'Sudaderas con capucha estilos únicos y de excelente calidad',
-      image: hoddie,
-    },
-  ],
-  mens: [
-    {
-      id: 1,
-      name: 'Sudadera New York',
-      price: 15,
-      description: 'Sudaderas con capucha estilos únicos y de excelente calidad',
-      image: hoddie,
-    },
-    {
-      id: 2,
-      name: 'Sudadera New York',
-      price: 15,
-      description: 'Sudaderas con capucha estilos únicos y de excelente calidad',
-      image: hoddie,
-    },
-    {
-      id: 3,
-      name: 'Sudadera New York',
-      price: 15,
-      description: 'Sudaderas con capucha estilos únicos y de excelente calidad',
-      image: hoddie,
+      image:
+        'https://images.unsplash.com/photo-1580331451062-99ff652288d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
     },
   ],
   products: [
