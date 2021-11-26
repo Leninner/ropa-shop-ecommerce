c504d1449a53bd87550ab83a4ff86cac83690b32
leninner <mazabandalenin180@gmail.com>
2021-11-25 21:02:07 -0500

    [UPDATE] añado solución para eliminar el item desde MyOrders

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import '../assets/styles/components/OrderItem.scss';
+import close from '../assets/images/icons/icon_close.png';
+import { deleteItemsFromCart } from '../actions';
+import { connect } from 'react-redux';
 
-const OrderItem = () => {
+const OrderItem = (props) => {
+  const { image, title, price } = props;
+
+  const handleDelete = () => {
+    props.deleteItemsFromCart(props.id);
+  };
+
   return (
     <div className='OrderItem'>
       <figure>
-        <img
-          src='https://images.unsplash.com/photo-1633861357509-77b7cd2bfe99?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-          alt=''
-        />
+        <img src={image} alt={title} />
       </figure>
-      <p>Producto Muestra</p>
-      <p>$,00</p>
-      <img src='' alt='X' className='close' />
+      <p>{title}</p>
+      <p>${price}</p>
+      <img src={close} alt='X' className='close' onClick={handleDelete} />
     </div>
   );
 };
 
-export default OrderItem;
+const mapDispatchToProps = {
+  deleteItemsFromCart,
+};
+
+export default connect(null, mapDispatchToProps)(OrderItem);
--- src/containers/MyOrders.jsx
+++ src/containers/MyOrders.jsx
@@ -1,35 +1,26 @@
 import React from 'react';
 import OrderItem from '../components/OrderItem';
 import '../assets/styles/containers/MyOrders.scss';
+import { connect } from 'react-redux';
 
-const MyOrders = () => {
+const MyOrders = ({ cart }) => {
+  let sumaTotal = 0;
+
   return (
     <aside className='MyOrder'>
       <h3 className='MyOrder__title'>Mi Carrito</h3>
       <div className='orders-container'>
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
+        {cart.length > 0
+          ? cart.map((item) => {
+              sumaTotal += item.price;
+              return <OrderItem key={item.id} {...item} />;
+            })
+          : 'No hay productos en el carrito'}
       </div>
       <div className='checkout'>
         <div className='checkout-total'>
           <h4>Total</h4>
-          <p className='checkout__price'>$0.00</p>
+          <p className='checkout__price'>${sumaTotal}</p>
         </div>
         <button className='checkout__button'>Iniciar proceso de Pago</button>
       </div>
@@ -37,4 +28,10 @@ const MyOrders = () => {
   );
 };
 
-export default MyOrders;
+const mapStateToProps = (state) => {
+  return {
+    cart: state.cart,
+  };
+};
+
+export default connect(mapStateToProps, null)(MyOrders);
