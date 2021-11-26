a16a60f767e0b799d74f77f8d6c421adff3a9e27
leninner <mazabandalenin180@gmail.com>
2021-11-26 17:24:37 -0500

    [UPDATE] files

--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import '../assets/styles/components/WhatsappButton.scss';
-import { connect } from 'react-redux';
 
-const WhatsappButton = () => {
-  return (
-    <>
-      {' '}
-      <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' />
-      <a href='https://walink.co/96e5fc' className='float' target='_blank' rel='noreferrer'>
-        <i className='fa fa-whatsapp my-float'></i>
-      </a>
-    </>
-  );
-};
+const WhatsappButton = () => (
+  <>
+    {' '}
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' />
+    <a href='https://walink.co/96e5fc' className='float' target='_blank' rel='noreferrer'>
+      <i className='fa fa-whatsapp my-float'></i>
+    </a>
+  </>
+);
 
-export default connect(null, null)(WhatsappButton);
+export default WhatsappButton;
