24bd3287c4187c9a598b14fd5dd154f89666972a
leninner <mazabandalenin180@gmail.com>
2021-12-08 18:54:29 -0500

    Update

--- src/components/CardCheckout.jsx
+++ src/components/CardCheckout.jsx
@@ -13,15 +13,14 @@ const CardCheckout = (props) => {
 
   return (
     <>
-      <section className='checkout-main'>
-        <h1>Carrito de Compras</h1>
-        <div className='card-checkout'>
-          <div className='card-checkout-img'>
-            <img src={image} alt='img' />
-          </div>
-          <div className='card-checkout-info'>
-            <h3>{name}</h3>
-            <p>{description}</p>
+      <div className='card-checkout'>
+        <div className='card-checkout-img'>
+          <img src={image} alt='img' />
+        </div>
+        <div className='card-checkout-info'>
+          <h3>{name}</h3>
+          <p>{description}</p>
+          <div className='prices'>
             <p>
               <span>Talla: </span>
               {talla}
@@ -31,17 +30,16 @@ const CardCheckout = (props) => {
               {cantidad}
             </p>
             <p>
-              <span>Precio Unitario: </span>
-              {price}
+              <span>Precio Unitario: </span>${price}
             </p>
           </div>
-          <div className='resume'>
-            <p>{price * cantidad}</p>
-            <MdRemoveShoppingCart size='35' onClick={() => handleDelete(id)} />
-          </div>
         </div>
-        <div className='form-checkout'></div>
-      </section>
+        <div className='resume'>
+          <p>${price * cantidad}</p>
+          <p>|</p>
+          <MdRemoveShoppingCart size='30' onClick={() => handleDelete(id)} />
+        </div>
+      </div>
     </>
   );
 };
--- src/containers/CheckoutList.jsx
+++ src/containers/CheckoutList.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import CardCheckout from '../components/CardCheckout';
 import { connect } from 'react-redux';
+import '../assets/styles/containers/CheckoutList.scss';
+import { Form } from '../components/Form';
 
 const CheckoutList = ({ cart }) => {
   return (
-    <div>
-      <h1>CheckoutList</h1>
-      {cart.map((value) => (
-        <CardCheckout key={value} {...value} />
-      ))}
+    <div className='checkout-container'>
+      <h1>Este es tu Carrito de Compras</h1>
+      <div className='mm'>
+        <div className='overflow--checkout'>
+          {cart.map((value) => (
+            <CardCheckout key={value} {...value} />
+          ))}
+        </div>
+        <Form />
+      </div>
     </div>
   );
 };
