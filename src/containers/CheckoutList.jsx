afe2d37b501c297f51f622f88b8c93833a518126
leninner <mazabandalenin180@gmail.com>
2021-12-09 09:09:41 -0500

    Update

--- src/containers/CheckoutList.jsx
+++ src/containers/CheckoutList.jsx
@@ -3,8 +3,11 @@ import CardCheckout from '../components/CardCheckout';
 import { connect } from 'react-redux';
 import '../assets/styles/containers/CheckoutList.scss';
 import Form from '../components/Form';
+import { useNavigate } from 'react-router-dom';
 
 const CheckoutList = ({ cart }) => {
+  const navigate = useNavigate();
+
   return (
     <div className='checkout-container'>
       <h1>Este es tu Carrito de Compras</h1>
@@ -13,6 +16,7 @@ const CheckoutList = ({ cart }) => {
           {cart.map((value) => (
             <CardCheckout key={value.id} {...value} />
           ))}
+          {!cart.length && navigate('/')}
         </div>
         <Form />
       </div>
