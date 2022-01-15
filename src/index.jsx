acd735783e26808a1ca661839a52cff9831da308
leninner <mazabandalenin180@gmail.com>
2022-01-15 09:43:45 -0500

    Update

--- src/index.jsx
+++ src/index.jsx
@@ -481,11 +481,7 @@ const initialState = {
       name: 'Pantalones',
       price: 15,
       description: 'Sudaderas con capucha estilos únicos y de excelente calidad',
-      images: [
-        'https://images.unsplash.com/photo-1541099649105-f69ad21f3246?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-        'https://media.istockphoto.com/photos/smiling-man-outdoors-in-the-city-picture-id1179420343?b=1&k=20&m=1179420343&s=170667a&w=0&h=xBR4ZQbRC1Ub_5u_pZnG-omh0k-W8QDQgXxda5MCDT8=',
-        'https://picsum.photos/200/300',
-      ],
+      images: [hoddie, hoddie, hoddie],
       cantidad: 1,
       talla: 'S',
       categorie: 'pantalones',
