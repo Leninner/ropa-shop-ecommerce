5edf4ee7d060bb0820ef64566493522c35eba6b1
leninner <mazabandalenin180@gmail.com>
2022-04-17 19:57:29 -0500

    Update styles and added slider for images

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,54 +1,27 @@
 import React from 'react';
 import { CardItem } from '../components/CardItem';
-import classNames from 'classnames';
 import '../assets/styles/containers/ProductList.scss';
-import Categories from '../components/Categories';
+// import Categories from '../components/Categories';
 import { useSelector } from 'react-redux';
 
-export const ProductList = ({ allItems, setModal, modal }) => {
-  const productListClass = classNames('product-list', {
-    allItems,
-  });
-  const { products, bestSellers, byCategory } = useSelector((state) => state);
+export const ProductList = () => {
+  const { products, bestSellers } = useSelector((state) => state);
 
   return (
-    <section className={productListClass} id='allProducts'>
-      {modal ? (
-        <>
-          <p className='feedback'>
-            {window.innerWidth > 768
-              ? 'Pasa el mouse por encima para ver más fotos'
-              : 'Da click a una imagen para ver más fotos'}
-          </p>
+    <section className='product-list'>
+      <p className='feedback'>
+        {window.innerWidth > 768 ? 'Pasa el mouse por encima para ver más fotos' : 'Scroll a las fotos'}
+      </p>
 
-          <div className='container__productList'>
-            {bestSellers.map((product) => {
-              return <CardItem key={product.id} product={product} bestSellers />;
-            })}
-          </div>
-          <button className='product-list_button' onClick={setModal}>
-            Ver más productos
-          </button>
-        </>
-      ) : (
-        <>
-          <Categories />
-          <p className='feedback'>Pasa el mouse por encima para ver más fotos</p>
-          {byCategory.length ? (
-            <div className='container__allProducts'>
-              {byCategory.map((product) => {
-                return <CardItem key={product.id} product={product} />;
-              })}
-            </div>
-          ) : (
-            <div className='container__allProducts'>
-              {products.map((product) => {
-                return <CardItem key={product.id} product={product} />;
-              })}
-            </div>
-          )}
-        </>
-      )}
+      <div className='container__productList'>
+        {bestSellers.map((product) => (
+          <CardItem key={product.id} product={product} bestSellers />
+        ))}
+
+        {products.map((product) => (
+          <CardItem key={product.id} product={product} />
+        ))}
+      </div>
     </section>
   );
 };
