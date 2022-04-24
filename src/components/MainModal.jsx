613528cc3ac466e310fd9ddb484bef1eeec9a0bf
leninner <mazabandalenin180@gmail.com>
2022-04-23 20:55:54 -0500

    Update

--- src/components/MainModal.jsx
+++ src/components/MainModal.jsx
@@ -2,6 +2,7 @@ import Slider from 'react-slick';
 import '../assets/styles/components/MainModal.scss';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import somePhoto from '../assets/images/mainView/fondoOne.png';
 
 export const MainModal = () => {
   const settings = {
@@ -21,46 +22,22 @@ export const MainModal = () => {
     <section className='mainModal'>
       <Slider {...settings} className='mainModal__slider'>
         <div>
-          <img
-            className='mainModal__img'
-            src='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__480.jpg'
-            alt='img'
-          />
+          <img className='mainModal__img' src={somePhoto} alt='img' />
         </div>
         <div>
-          <img
-            className='mainModal__img'
-            src='https://cdn.pixabay.com/photo/2021/10/19/10/56/cat-6723256__340.jpg'
-            alt='img'
-          />
+          <img className='mainModal__img' src={somePhoto} alt='img' />
         </div>
         <div>
-          <img
-            className='mainModal__img'
-            src='https://cdn.pixabay.com/photo/2014/03/12/01/36/eyes-285825__340.png'
-            alt='img'
-          />
+          <img className='mainModal__img' src={somePhoto} alt='img' />
         </div>
         <div>
-          <img
-            className='mainModal__img'
-            src='https://cdn.pixabay.com/photo/2020/06/20/11/08/siamese-cat-5320568__340.jpg'
-            alt='img'
-          />
+          <img className='mainModal__img' src={somePhoto} alt='img' />
         </div>
         <div>
-          <img
-            className='mainModal__img'
-            src='https://cdn.pixabay.com/photo/2021/08/23/17/53/cat-6568422__340.jpg'
-            alt='img'
-          />
+          <img className='mainModal__img' src={somePhoto} alt='img' />
         </div>
         <div>
-          <img
-            className='mainModal__img'
-            src='https://cdn.pixabay.com/photo/2017/04/11/15/55/animals-2222007__340.jpg'
-            alt='img'
-          />
+          <img className='mainModal__img' src={somePhoto} alt='img' />
         </div>
       </Slider>
     </section>
