1b925ec5db67a6881859a5091af44ded210a9ccf
luiscrack22 <luisquinga91@gmail.com>
2022-04-25 16:07:34 -0500

    uptade

--- src/components/MainModal.jsx
+++ src/components/MainModal.jsx
@@ -2,7 +2,7 @@ import Slider from 'react-slick';
 import '../assets/styles/components/MainModal.scss';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import portada1 from '../assets/images/mainView/portada-r1.png';
+import portada from '../assets/images/mainView/portada.png';
 
 export const MainModal = () => {
   const settings = {
@@ -22,7 +22,7 @@ export const MainModal = () => {
     <section className='mainModal'>
       <Slider {...settings} className='mainModal__slider'>
         <div>
-          <img className='mainModal__img' src={portada1} alt='img' />
+          <img className='mainModal__img' src={portada} alt='img' />
         </div>
         <div>
           <img className='mainModal__img' src={somePhoto} alt='img' />
