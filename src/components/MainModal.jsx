969705a26db6b8dfb08828f250e4751054724e4c
luiscrack22 <luisquinga91@gmail.com>
2022-08-18 22:29:32 -0500

    update

--- src/components/MainModal.jsx
+++ src/components/MainModal.jsx
@@ -25,7 +25,7 @@ export const MainModal = () => {
         <div>
           <img
             className='main_Modal__img'
-            src='https://images.unsplash.com/photo-1560243563-062bfc001d68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cm9wYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+            src=''
             alt='img'
           />
         </div>
