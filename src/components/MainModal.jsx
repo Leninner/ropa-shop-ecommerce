a32f200ae53e2ac06e57504fa1fa28aaef1ae325
luiscrack22 <luisquinga91@gmail.com>
2022-08-20 22:43:50 -0500

    update

--- src/components/MainModal.jsx
+++ src/components/MainModal.jsx
@@ -1,6 +1,6 @@
 import Slider from 'react-slick';
 import portada2 from '../assets/images/mainView/portada2.png';
-import portada3 from '../assets/images/mainView/port3.jpg';
+import portada3 from '../assets/images/mainView/port3.gif';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import '../styles/components/MainModal.scss';
